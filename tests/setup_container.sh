1692124772eb54e9b03587d4fb0c1ea4b7a83bed
Herb Lainchbury <herb@dynamic-solutions.com>
2021-07-01 11:16:20 -0700

    add test environment variables

--- tests/setup_container.sh
+++ tests/setup_container.sh
@@ -1,3 +1,7 @@
 
 pip install .
 pip install -r tests/requirements.txt
+
+export GITDATA_TEST_DATABASE_HOST=mariadb
+export GITDATA_TEST_DATABASE_USER=root
+export GITDATA_TEST_DATABASE_PASSWORD=root
