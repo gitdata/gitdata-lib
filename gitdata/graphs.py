c4dce786b802a1a3c91b67f27d1d31f3d08e3d8f
Herb Lainchbury <herb@dynamic-solutions.com>
2021-12-29 10:12:53 -0800

    add graphs module

---
+++ gitdata/graphs.py
@@ -0,0 +1,321 @@
+"""
+    gitdata graph
+"""
+
+import base64
+import datetime
+from decimal import Decimal
+from datetime import datetime, date
+import io
+
+import gitdata
+from gitdata.digester import digested, undigested
+from gitdata.buckets import FileBucket, MemoryBucket
+from gitdata.stores.facts import FactStore
+
+def retype(value, value_type):
+    """convert a value back to its original type"""
+    if value_type == 'str':
+        pass
+
+    elif value_type == "int":
+        value = int(value)
+
+    elif value_type == 'float':
+        value = float(value)
+
+    elif value_type == 'decimal.Decimal':
+        value = Decimal(value)
+
+    elif value_type == "datetime.date":
+        y = int(value[:4])
+        m = int(value[5:7])
+        d = int(value[8:10])
+        value = date(y, m, d)
+
+    elif value_type == "datetime":
+        y = int(value[:4])
+        m = int(value[5:7])
+        d = int(value[8:10])
+        hr = int(value[11:13])
+        mn = int(value[14:16])
+        sc = int(value[17:19])
+        value = datetime(y, m, d, hr, mn, sc)
+
+    elif value_type == "stream":
+        None
+
+    elif value_type == "datetime.datetime":
+        y = int(value[:4])
+        m = int(value[5:7])
+        d = int(value[8:10])
+        hr = int(value[11:13])
+        mn = int(value[14:16])
+        sc = int(value[17:19])
+        value = datetime(y, m, d, hr, mn, sc)
+
+    elif value_type == 'bool':
+        value = (value == '1' or value == 'True')
+
+    elif value_type == 'NoneType':
+        value = None
+
+    elif value_type == 'bytes':
+        value = base64.b64decode(value)
+
+    else:
+        msg = 'unsupported data type: ' + repr(value_type)
+        raise Exception(msg)
+
+    return value
+
+
+class AbstractFactStore(object):
+    """Abstract Fact Store"""
+
+    def add(self, facts):
+        """add facts to the fact store"""
+
+    def remove(self, facts):
+        """remove facts from the fact store"""
+
+    def matching(self, pattern):
+        """return facts that match the pattern"""
+
+    def put(self, entity):
+        """put an entity into the fact store"""
+
+    def get(self, uid):
+        """get an entity from the fact store"""
+
+    def delete(self, uid):
+        """delete an entity from the fact store"""
+
+    def clear(self):
+        """delete all facts from the fact store"""
+
+    def __len__(self):
+        """return the number of facts stored"""
+
+    def __repr__(self):
+        pattern = (None, None, None)
+        return '{}({})'.format(
+            self.__class__.__name__,
+            ', '.join(
+                repr(fact)
+                for fact in self.matching(pattern)
+            )
+        )
+
+    def __str__(self):
+        pattern = (None, None, None)
+        return '\n'.join(repr(fact) for fact in self.matching(pattern))
+
+
+# class Node:
+#     """Graph Node"""
+
+#     graph = None
+#     uid = None
+
+#     def __init__(self, **kwargs):
+#         self.__dict__.update(kwargs)
+
+#     def add(self, relation, data):
+#         """Add a related data to a node"""
+#         uid = self.graph.add(data)
+#         self.graph.store.add([(self.uid, relation, uid)])
+#         return uid
+
+#     def delete(self):
+#         """Delete all facts related to a node"""
+#         self.graph.store.remove(
+#             self.graph.triples((self.uid, None, None))
+#         )
+
+#     def __getitem__(self, name):
+#         values = self.graph.triples((self.uid, name, None))
+#         return values[0][-1] if values else None
+
+#     def __getattr__(self, name):
+#         values = self.graph.triples((self.uid, name, None))
+#         return values[0][-1] if values else None
+
+#     def keys(self):
+#         for k, _ in self.items():
+#             yield k
+
+#     def values(self):
+#         for _, v in self.items():
+#             yield v
+
+#     def items(self):
+#         for _, p, o in self.graph.triples((self.uid, None, None)):
+#             if self.graph.get(o):
+#                 o = Node(graph=self.graph, uid=o)
+#             result = p, o
+#             yield result
+
+#     def __eq__(self, value):
+#         return dict(self.items()) == dict(value)
+
+#     def __iter__(self):
+#         members = self.graph.query([
+#             (self.uid, None, '_a'),
+#             ('_a', 'includes', '?uid')
+#         ])
+#         if members:
+#             for member in members:
+#                 yield Node(graph=self.graph, **member)
+
+#         if self.graph.triples((self.uid, 'includes', None)):
+#             for _, _, o in self.graph.triples((self.uid, 'includes', None)):
+#                 yield Node(graph=self.graph, uid=o)
+#         return self.keys()
+
+#     def __str__(self):
+
+#         members = [o for s, p, o in self.graph.triples((self.uid, 'includes', None))]
+#         if members:
+#             return repr(tuple(Node(graph=self.graph, uid=n) for n in members))
+
+#         name = '{}({})'.format(
+#             self.__class__.__name__,
+#             self.uid
+#         )
+
+#         t = ['  {} {}: {!r}'.format(
+#             key,
+#             '.'*(20-len(key[:20])),
+#             value,
+#         ) for key, value in self.items()]
+
+#         return '\n'.join([name] + t)
+
+#     def __repr__(self):
+#         # pattern = (self.uid, None, None)
+#         members = [o for s, p, o in self.graph.triples((self.uid, 'includes', None))]
+#         if members:
+#             return repr(tuple(Node(graph=self.graph, uid=n) for n in members))
+#         return f'Node({self.uid})'
+
+
+class Graph:
+    """Basic Graph"""
+
+    def __init__(self, location=None, new_uid=gitdata.utils.new_uid):
+        self.db = gitdata.database.connect()
+        self.facts = FactStore(new_uid=new_uid)
+        self.new_uid = new_uid
+
+    def add(self, data):
+        """Add data to the graph"""
+        return self.facts.add(digested(data, new_uid=self.new_uid))
+
+    def clear(self):
+        """Remove all facts from the graph"""
+        self.facts.clear()
+
+    def delete(self, pattern):
+        """Delete all facts matching the pattern"""
+        facts = self.facts.triples(pattern)
+        self.facts.remove(facts)
+
+    def get(self, uids):
+        """Get a node of the graph"""
+
+        as_list = True
+        if not isinstance(uids, (list, tuple, set)):
+            uids = [uids]
+            as_list = False
+
+        result = []
+        for uid in sorted(uids):
+            values = {k: v for _, k, v in self.facts.triples((uid, None, None))}
+            if values:
+                result.append(dict(**values))
+
+        if result:
+            if as_list:
+                return result
+            return result[0]
+
+    def query(self, clauses):
+        """Query the graph"""
+        facts = self.facts
+        bindings = None
+        for clause in clauses:
+            bpos = {}
+            qc = []
+            for pos, x in enumerate(clause):
+                if isinstance(x, str) and (x.startswith('?') or x.startswith('_')):
+                    qc.append(None)
+                    bpos[x] = pos
+                else:
+                    qc.append(x)
+            rows = list(facts.triples((qc[0], qc[1], qc[2])))
+            # print(rows)
+            if bindings is None:
+                # This is the first pass, everything matches
+                bindings = []
+                for row in rows:
+                    binding = {}
+                    for var, pos in bpos.items():
+                        binding[var] = row[pos]
+                    bindings.append(binding)
+            else:
+                # in subsequent passes, eliminate bindings that dont work
+                newb = []
+                for binding in bindings:
+                    for row in rows:
+                        validmatch = True
+                        tempbinding = binding.copy()
+                        for var, pos in bpos.items():
+                            if var in tempbinding:
+                                if tempbinding[var] != row[pos]:
+                                    validmatch = False
+                            else:
+                                tempbinding[var] = row[pos]
+                        if validmatch:
+                            newb.append(tempbinding)
+                bindings = newb
+        return [
+            dict(
+                (k[1:], v)
+                for k, v in b.items()
+                if k[0] == '?'
+            ) for b in bindings
+        ] if bindings else []
+
+    def find(self, *args, **kwargs):
+        """Find nodes"""
+        query = []
+        for i in args:
+            query.append(('?subject', i, '?'+i))
+        for k, v in kwargs.items():
+            query.append(('?subject', k, v))
+        subjects = set(record['subject'] for record in self.query(query))
+        result = self.get(subjects) or []
+        return result
+
+    def first(self, *args, **kwargs):
+        """Find first node"""
+        result = self.find(*args, **kwargs)
+        for record in result:
+            return record
+
+    def exists(self, *args, **kwargs):
+        """Return True if specified nodes exist else return False"""
+        return bool(self.first(*args, **kwargs))
+
+    def __str__(self):
+        """Human friendly string representation"""
+        return '\n'.join(repr(triple) for triple in self.facts.triples())
+
+    def __len__(self):
+        return len(self.facts)
+
+
+# def get_graph(db=None):
+#     db = db or gitdata.database.connect()
+#     return Graph(db)
---
+++ tests/unittests/test_graphs.py
@@ -0,0 +1,366 @@
+"""
+    graph tests
+"""
+# pylint: disable=missing-docstring, no-member
+
+import datetime
+from decimal import Decimal
+import io
+
+import unittest
+
+from gitdata.graphs import Graph
+from gitdata.utils import test_uid_maker
+
+data = [
+    dict(
+        users=[
+            dict(
+                kind='user',
+                name='Joe',
+                birthdate=datetime.date(1991, 1, 2)
+            ),
+            dict(
+                kind='user',
+                name='Sally',
+                birthdate=datetime.date(1991, 1, 2),
+            ),
+        ]
+    ),
+    dict(
+        projects=[
+            dict(
+                kind='project',
+                name='Project One',
+                created=datetime.datetime(2019, 5, 2)
+            ),
+            dict(
+                kind='project',
+                name='Project Two',
+                created=datetime.datetime(2019, 5, 3)
+            ),
+        ]
+    ),
+]
+
+
+class GraphTests(unittest.TestCase):
+    """Graph Tests"""
+
+    def setUp(self):
+        self.graph = Graph(new_uid=test_uid_maker())
+        self.graph.clear()
+
+    def tearDown(self):
+        self.graph.clear()
+
+    def test_empty_graph(self):
+        g = self.graph
+        self.assertEqual(g.facts.triples((None, None, None)), [])
+
+    def test_add(self):
+        g = self.graph
+        g.add([dict(name='Joe', age=20), dict(name='Pat', wage=Decimal('12.1'))])
+        person = g.get('3')
+        self.assertEqual(person['wage'], Decimal('12.1'))
+        self.assertEqual(person['name'], 'Pat')
+
+    def test_stream_attribute(self):
+        g = self.graph
+        g.add([
+            dict(name='Joe', age=20, image=io.BytesIO(b'image')),
+            dict(name='Pat', wage=Decimal('12.1'))
+        ])
+        self.assertEqual(
+            g.get('2')['image'].read(),
+            b'image'
+        )
+
+    def test_query(self):
+        g = self.graph
+        g.add(data)
+        result = g.query([
+            (None, 'name', '?name')
+        ])
+        self.assertEqual(len(result), 4)
+
+    def test_find(self):
+        g = self.graph
+        g.add(data)
+        result = g.find('name')
+        self.assertEqual(len(result), 4)
+
+    def test_first(self):
+        g = self.graph
+        g.add(data)
+        result = g.first('name')
+        self.assertEqual(result['birthdate'], datetime.date(1991, 1, 2))
+
+    def test_add_none(self):
+        g = self.graph
+        g.add(None)
+        self.assertEqual(len(g), 0)
+
+    def test_add_value(self):
+        g = self.graph
+        g.add('test')
+        self.assertEqual(len(g), 0)
+
+    def test_add_dict(self):
+        g = self.graph
+        g.add(dict(value=2))
+        self.assertEqual(g.facts.triples(), [('1', 'value', 2)])
+
+    def test_add_list(self):
+        g = self.graph
+        g.add([1, 2, 3])
+        self.assertEqual(g.facts.triples((None, None, None)), [
+            ('1', 'includes', 1),
+            ('1', 'includes', 2),
+            ('1', 'includes', 3)
+        ])
+
+    def test_add_project(self):
+        g = self.graph
+        project_attributes = dict(
+            name='Sample',
+            kind='project',
+            created=datetime.datetime(2019, 6, 10),
+            created_by=1,
+        )
+        g.add(project_attributes)
+        self.assertEqual(g.facts.triples(), [
+            ('1', 'name', 'Sample'),
+            ('1', 'kind', 'project'),
+            ('1', 'created', datetime.datetime(2019, 6, 10)),
+            ('1', 'created_by', 1)
+        ])
+
+    def test_add_multiple(self):
+        g = self.graph
+        g.add(dict(data='123'))
+        g.add(dict(data='234'))
+        self.assertEqual(g.facts.triples(), [('1', 'data', '123'), ('2', 'data', '234')])
+
+    def test_convert_to_dict(self):
+        g = self.graph
+        joe = dict(name='Joe', age=20)
+        sam = dict(name='Sam', age=80)
+        g.add(joe)
+        g.add(sam)
+        node = g.first(name='Joe')
+        self.assertEqual(dict(node), joe)
+
+    # def test_add_project_attribute(self):
+    #     g = self.graph
+    #     project_attributes = dict(
+    #         name='Sample',
+    #         kind='project',
+    #         created=datetime.datetime(2019, 6, 10),
+    #         created_by=1,
+    #     )
+    #     g.add(project_attributes)
+    #     node = g.first(name='Sample')
+    #     self.assertEqual(node['kind'], 'project')
+
+    #     node.add('status', 'draft')
+    #     self.assertEqual(g.facts.triples(), [
+    #         ('1', 'name', 'Sample'),
+    #         ('1', 'kind', 'project'),
+    #         ('1', 'created', datetime.datetime(2019, 6, 10)),
+    #         ('1', 'created_by', 1),
+    #         ('1', 'status', 'draft'),
+    #     ])
+
+    # def test_add_project_attribute_as_list(self):
+    #     g = self.graph
+    #     project_attributes = dict(
+    #         name='Sample',
+    #         kind='project',
+    #         created=datetime.datetime(2019, 6, 10),
+    #         created_by=1,
+    #     )
+    #     g.add(project_attributes)
+    #     node = g.first(name='Sample')
+
+    #     node.add('cities', ['Vancouver', 'Victoria'])
+    #     self.assertEqual(g.triples(), [
+    #         ('1', 'name', 'Sample'),
+    #         ('1', 'kind', 'project'),
+    #         ('1', 'created', datetime.datetime(2019, 6, 10)),
+    #         ('1', 'created_by', 1),
+    #         ('2', 'includes', 'Vancouver'),
+    #         ('2', 'includes', 'Victoria'),
+    #         ('1', 'cities', '2'),
+    #     ])
+
+    # def test_add_project_attribute_as_list_of_objects(self):
+    #     g = self.graph
+    #     project_attributes = dict(
+    #         name='Sample',
+    #         kind='project',
+    #         created=datetime.datetime(2019, 6, 10),
+    #         created_by=1,
+    #     )
+    #     g.add(project_attributes)
+    #     node = g.first(name='Sample')
+
+    #     uid = g.add([
+    #         dict(name='Vancouver'),
+    #         dict(name='Victoria'),
+    #     ])
+
+    #     attribute = (node.uid, 'cities', uid)
+    #     g.store.add([attribute])
+    #     self.assertEqual(g.triples(), [
+    #         ('1', 'name', 'Sample'),
+    #         ('1', 'kind', 'project'),
+    #         ('1', 'created', datetime.datetime(2019, 6, 10)),
+    #         ('1', 'created_by', 1),
+    #         ('3', 'name', 'Vancouver'),
+    #         ('2', 'includes', '3'),
+    #         ('4', 'name', 'Victoria'),
+    #         ('2', 'includes', '4'),
+    #         ('1', 'cities', '2'),
+    #     ])
+
+    # def test_add_list_of_objects_to_node(self):
+    #     g = self.graph
+    #     project_attributes = dict(
+    #         name='Sample',
+    #         kind='project',
+    #         created=datetime.datetime(2019, 6, 10),
+    #         created_by=1,
+    #     )
+    #     g.add(project_attributes)
+    #     node = g.first(name='Sample')
+
+    #     node.add('cities', [
+    #         dict(name='Vancouver'),
+    #         dict(name='Victoria'),
+    #     ])
+
+    #     self.assertEqual(g.triples(), [
+    #         ('1', 'name', 'Sample'),
+    #         ('1', 'kind', 'project'),
+    #         ('1', 'created', datetime.datetime(2019, 6, 10)),
+    #         ('1', 'created_by', 1),
+    #         ('3', 'name', 'Vancouver'),
+    #         ('2', 'includes', '3'),
+    #         ('4', 'name', 'Victoria'),
+    #         ('2', 'includes', '4'),
+    #         ('1', 'cities', '2'),
+    #     ])
+
+
+
+
+class GraphTestsPreloaded(unittest.TestCase):
+    """Graph Tests with Preloaded Data"""
+
+    def setUp(self):
+        self.graph = Graph(new_uid=test_uid_maker())
+        # self.graph = gitdata.Graph(store, new_uid=new_test_uid(0))
+        self.graph.clear()
+        self.graph.add(data)
+
+    def test_get(self):
+        n4 = self.graph.get('4')
+        self.assertEqual(dict(n4), dict(
+            name='Joe',
+            kind='user',
+            birthdate=datetime.date(1991, 1, 2)
+            )
+        )
+
+    def test_getitem(self):
+        user = self.graph.get('4')
+        self.assertEqual(user['name'], 'Joe')
+
+    def test_first_again(self):
+        project = dict(
+            kind='project',
+            name='Project One',
+            created=datetime.datetime(2019, 5, 2)
+        )
+        project1 = self.graph.first(kind='project')
+        self.assertEqual(project1, project)
+
+    def test_len(self):
+        self.assertEqual(len(self.graph), 20)
+
+    def test_delete(self):
+        g = self.graph
+
+        self.assertEqual(g.query([
+            ('?uid', 'name', '?name'),
+            ('?uid', 'kind', 'user'),
+        ]), [
+            {'uid': '4', 'name': 'Joe'},
+            {'uid': '5', 'name': 'Sally'}
+        ])
+
+        g.delete((None, 'name', 'Joe'))
+
+        self.assertEqual(g.query([
+            ('?uid', 'name', '?name'),
+            ('?uid', 'kind', 'user'),
+        ]), [
+            {'uid': '5', 'name': 'Sally'}
+        ])
+
+    def test_first_missing(self):
+        answer = self.graph.first(kind='animal')
+        self.assertEqual(answer, None)
+
+    def test_first(self):
+        project1 = self.graph.first(kind='project')
+        self.assertEqual(project1['name'], 'Project One')
+
+    def test_exists(self):
+        g = self.graph
+        self.assertTrue(g.exists(kind='project'))
+        g.delete((None, 'kind', 'project'))
+        self.assertFalse(g.exists(kind='project'))
+
+    def test_query(self):
+        g = self.graph
+        answer = g.query([
+            ('?uid', 'name', '?name'),
+            ('?uid', 'kind', 'user'),
+        ])
+        self.assertEqual(answer, [
+            {'uid': '4', 'name': 'Joe'},
+            {'uid': '5', 'name': 'Sally'}
+        ])
+
+    def test_find(self):
+        g = self.graph
+        answer = g.find(kind='project')
+        self.assertEqual(len(answer), 2)
+        self.assertEqual(answer[1]['name'], 'Project Two')
+
+    def test_find_missing(self):
+        g = self.graph
+        answer = g.find(kind='animal')
+        self.assertEqual(answer, [])
+
+    # def test_iter_node_list(self):
+    #     users = self.graph.get('2')
+    #     print(users)
+    #     names = [user.name for user in users]
+    #     self.assertEqual(names, ['Joe', 'Sally'])
+
+    # def test_traversal(self):
+    #     Node = gitdata.Node
+    #     g = self.graph
+    #     print(g)
+    #     print(g.first(name='Joe'))
+    #     n = Node(graph=self.graph, uid='4')
+    #     print(n)
+    #     print(g.get('1'))
+    #     self.assertEqual(
+    #         n.projects, None
+    #     )
+
+
